59c8cc90dc8a8ed7ece1f280f4d33b3c07d6cd2d
Darshan S <darshans2727@gmail.com>
2025-12-24 20:35:32 +0530

    refactor(ui): adjust live status badge padding for consistent sizing

--- src/renderer/src/pages/components/media-info/live-status.tsx
+++ src/renderer/src/pages/components/media-info/live-status.tsx
@@ -3,11 +3,11 @@ import { MediaInfoJson } from '@/shared/types/info-json';
 const LiveStatus = ({ infoJson }: { infoJson: MediaInfoJson }) => {
   if (infoJson.was_live) {
     return (
-      <span className="bg-red-500 text-white text-[12px] px-2 py-0.5 rounded-full">Was Live</span>
+      <span className="bg-red-500 text-white text-[12px] p-1 px-2 rounded-full">Was Live</span>
     );
   } else if (infoJson.is_live) {
     return (
-      <span className="bg-red-500 text-white text-[12px] px-2 py-0.5 rounded-full">Live Now</span>
+      <span className="bg-red-500 text-white text-[12px] p-1 px-2 rounded-full">Live Now</span>
     );
   } else return null;
 };
