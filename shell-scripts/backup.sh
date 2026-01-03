a1fb80420438e07b1484d4db73c9bd51e70afe2d
Darshan S <darshans2727@gmail.com>
2026-01-03 19:35:43 +0530

    chore: add mock and backup scripts, update dependencies

---
+++ shell-scripts/backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p ./mock
+cp -rp ./app-data/User/media-data ./mock
+cp -rp ./app-data/User/app.db ./mock/app.db
---
+++ shell-scripts/mock.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -rp ./mock/media-data ./app-data/User
+cp -rp ./mock/app.db ./app-data/User/app.db
